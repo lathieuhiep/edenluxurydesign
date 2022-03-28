cb95f46a25151c0171ee7ff31b7ce2dd8d8f4a9d
LaThieuHiep <khacdiepkma90@gmail.com>
2022-03-29 01:03:44 +0700

    style content

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -202,6 +202,65 @@
             })
         }
 
+        // project slider
+        const elementProjectSlider = $('.element-project-slider');
+        if ( elementProjectSlider.length ) {
+
+            elementProjectSlider.each(function () {
+                const $this = $(this);
+
+                $this.owlCarousel({
+                    loop: true,
+                    margin: 18,
+                    nav: false,
+                    dots: true,
+                    navSpeed: 800,
+                    dotsSpeed: 800,
+                    dragEndSpeed: 800,
+                    responsive:{
+                        0:{
+                            items: 1
+                        },
+                        600:{
+                            items: 3
+                        },
+                        1000:{
+                            items: 5
+                        }
+                    },
+                    onInitialized: function () {
+                        const owlItemActive = $this.find('.owl-item.active');
+                        const first = owlItemActive[0];
+                        const last = owlItemActive[owlItemActive.length - 2];
+
+                        first.classList.add('overlay');
+                        last.classList.add('overlay');
+                    },
+                    onDrag: function () {
+                        const owlItem = $this.find('.owl-item');
+                        owlItem.removeClass('overlay');
+                    },
+                    onTranslate: function () {
+                        const owlItemActive = $this.find('.owl-item.active');
+                        owlItemActive.removeClass('overlay');
+                    },
+                    onTranslated: function () {
+                        const owlItemActive = $this.find('.owl-item.active');
+                        const first = owlItemActive[0];
+                        let last = owlItemActive[owlItemActive.length - 2];
+
+                        if ( owlItemActive.length === 5 ) {
+                            last = owlItemActive[owlItemActive.length - 1];
+                        }
+
+                        first.classList.add('overlay');
+                        last.classList.add('overlay');
+                    }
+                })
+            })
+
+        }
+
     });
 
 } )(jQuery);
